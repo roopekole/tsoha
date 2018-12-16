e8d6181caec15cb00dc38af0907da149c6e5bb64
Kolehmainen Roope <roope.kolehmainen@opuscapita.com>
2018-12-16 17:50:26 +0200

    Add current_user import to Science class

--- application/science/views.py
+++ application/science/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, current_user
 
 from application import app, db, login_required
 from application.science.models import Science
